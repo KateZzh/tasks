49fe62e6174e1aecb098cbb4be7e96f06203864d
KateZzh <ekaterin.pro2@gmail.com>
2023-01-11 23:56:00 +0300

    added 7 tasks

--- 013.numbers/11/app.js
+++ 013.numbers/11/app.js
@@ -1,1 +1,13 @@
+// Напишите программу, в которой вычисляется сумма, разность и произведение
+// двух целых чисел, введенных с клавиатуры.
 
+const firstNumber = +prompt("Введите первое число:");
+const secondNumber = +prompt("Введите второе число:");
+
+if (firstNumber % 1 === 0 && secondNumber % 1 === 0) {
+  console.log(firstNumber + secondNumber);
+  console.log(firstNumber - secondNumber);
+  console.log(firstNumber * secondNumber);
+} else {
+  console.log("Некорректный ввод");
+}
---
+++ 013.numbers/14/app.js
@@ -0,0 +1,10 @@
+// Вы вводите значение с клавиатуры. Если значение четное, то вывести true, в
+// противном случае false
+
+const number = +prompt("Введите число:");
+
+if (number % 2 === 0) {
+  console.log("true");
+} else {
+  console.log("false");
+}
