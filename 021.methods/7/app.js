3b46e5300f868b2ea6667ff434c84cbd8f3452e2
KateZzh <ekaterin.pro2@gmail.com>
2023-02-02 00:08:49 +0300

    some changes

--- 021.methods/7/app.js
+++ 021.methods/7/app.js
@@ -5,3 +5,5 @@ const arr = ["hschool", "hschool_0", "hschool_1"];
 
 console.log(arr[0], arr[2]);
 console.log(arr[arr.length - 3], arr[arr.length - 1]);
+console.log(arr.indexOf("hschool"), arr.lastIndexOf("hschool_1"));
+console.log(arr.slice(0, 1), arr.slice(2));
