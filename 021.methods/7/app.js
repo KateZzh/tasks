ddc87a11c0ff422133423e177ee8db4a87681baf
KateZzh <ekaterin.pro2@gmail.com>
2023-02-01 23:52:32 +0300

    added 4 tasks

---
+++ 021.methods/7/app.js
@@ -0,0 +1,7 @@
+// На входе задан массив [‘hschool’, ‘hschool_0’, ‘hschool_1’]. Напишите все возможные
+// способы обращения к первому и последнему элементам
+
+const arr = ["hschool", "hschool_0", "hschool_1"];
+
+console.log(arr[0], arr[2]);
+console.log(arr[arr.length - 3], arr[arr.length - 1]);
