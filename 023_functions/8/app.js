728b177d1775a19c6183aaaddb6dcae1d10ed1ca
KateZzh <ekaterin.pro2@gmail.com>
2023-02-10 22:50:15 +0300

    fix

--- 023_functions/8/app.js
+++ 023_functions/8/app.js
@@ -3,67 +3,26 @@
 // Если результат функции проверки – true, то вызывать новую функцию,
 // возвращающую конкатенацию всех строчных элементов массива
 
-const arr = ["a", "b", "  ", "c"];
+const arr = ["a", "b", "c", " ", "5"];
 
 function checkArrStr(arr_) {
-    let countNum = 0;
-  
-    for (let i = 0; i < arr_.length; i++) {
-      if (isNaN(arr_[i])) {
-  console.log(true);
-      } else {
-        console.log(false);
-      }
+  const bool = arr_.every((el) => (typeof el == "string" ? true : false));
+  return bool;
+}
+
+function sumArrStr(arr_) {
+  let newArr = "";
+  for (let i = 0; i < arr_.length; i++) {
+    const bool = checkArrStr(arr_);
+    if (bool === true) {
+      newArr += arr_[i];
+    } else {
+      console.log("error");
+      break;
     }
-  
-  
   }
-  checkArrStr(arr);
+  return newArr;
+}
 
-// function checkArrStr(arr_) {
-//   let countNum = 0;
-
-//   for (let i = 0; i < arr_.length; i++) {
-//     if (isNaN(arr_[i])) {
-//       continue;
-//     } else {
-//       countNum++;
-//     }
-//   }
-
-//   if (countNum > 0) {
-//     return false;
-//   } else return true;
-// }
-
-//   const res = arr_.every(function (el) {
-//     if (isNaN(el)) {
-//       return true;
-//     } else {
-//       return false;
-//     }
-//   });
-//   return res;
-
-// resArrStr = checkArrStr(arr);
-// console.log(resArrStr);
-
-// function sumArrStr(arr_) {
-//   let newArr = "";
-//   for (let i = 0; i < arr_.length; i++) {
-//     if (resArrStr === true) {
-//       newArr += arr_[i];
-//     } else {
-//       console.log("error");
-//       break;
-//     }
-//   }
-//   return newArr;
-// }
-
-// const result = sumArrStr(arr);
-// console.log(result);
-
-
-
-
+const result = sumArrStr(arr);
+console.log(result);
