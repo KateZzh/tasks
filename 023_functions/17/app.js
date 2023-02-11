8a6489117cdd6cc6ebc03d5f4e5adb10859a2e23
KateZzh <ekaterin.pro2@gmail.com>
2023-02-11 23:24:00 +0300

    added 1 task

---
+++ 023_functions/17/app.js
@@ -0,0 +1,30 @@
+// Написать функцию, принимающую в параметрах строку текста в маленьком
+// регистре. Разбить строку на массив. Функция должна вернуть строку, где каждый
+// элемент массива в чередование регистров toLowerCase, toUpperCase
+// hschool -> HsChOoL
+
+const str = "hschool";
+
+// function alternRegStr(str_) {
+//   const arr = str_.split("");
+//   let res = "";
+//   for (let i = 0; i < arr.length; i++) {
+//     i % 2 === 0 ? (res += arr[i].toUpperCase()) : (res += arr[i]);
+//   }
+//   return res;
+// }
+
+// const result = alternRegStr(str);
+// console.log(result);
+
+const alternRegStr = (str_) => {
+  const arr = str_.split("");
+  let res = "";
+
+  for (let i = 0; i < arr.length; i++) {
+    i % 2 === 0 ? (res += arr[i].toUpperCase()) : (res += arr[i]);
+  }
+  return res;
+};
+
+console.log(alternRegStr(str));
