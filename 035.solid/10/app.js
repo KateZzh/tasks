bbfc936c7b281d77b555a28b13a29f90836b1e6c
KateZzh <ekaterin.pro2@gmail.com>
2023-03-25 23:54:23 +0300

    add 035: 10 task

---
+++ 035.solid/10/app.js
@@ -0,0 +1,37 @@
+// Реализуйте класс Сonversion, сдержащий метод doBinaryNumber, преобразующий
+// все числа 2 системы счисления в 10
+
+class Conversion {
+  doBinaryNumber(binaryNum) {
+    try {
+      const arrBinaryNum = binaryNum.split("");
+
+      if (!this.isValid(arrBinaryNum))
+        throw new Error(
+          "Число содержит символы, отсутствующие в выбранной системе счисления"
+        );
+
+      let numDecimalNumber = 0;
+      let count = 0;
+
+      for (let i = arrBinaryNum.length - 1; i >= 0; i--) {
+        numDecimalNumber += arrBinaryNum[count] * 2 ** i;
+        count++;
+      }
+
+      console.log(numDecimalNumber);
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  isValid(arrBinaryNum) {
+    const bool = arrBinaryNum.every((el) => el == 0 || el == 1);
+    return bool;
+  }
+}
+
+const binaryNum = "101110111";
+
+const conversion = new Conversion();
+conversion.doBinaryNumber(binaryNum);
