965c35c30daf51d4992ebf4db699482f5b65681c
KateZzh <ekaterin.pro2@gmail.com>
2024-03-16 21:25:41 +0300

    086.repeat: task16

--- 086.repeat/task16/app.js
+++ 086.repeat/task16/app.js
@@ -5,27 +5,41 @@
 // двух чисел в любом порядке.
 // Если решения нет, вернуть пустой массив
 
+// const findPairsWithSum = (arr, sum) => {
+//   let res = [];
+//   const newArr = [...arr];
+
+//   for (let j = 0; j < arr.length; j++) {
+//     for (let i = 1; i < newArr.length; i++) {
+//       if (newArr[i] + arr[j] === sum) {
+//         res.push(newArr[i]);
+//         break;
+//       }
+//     }
+
+//     newArr.shift(newArr[0]);
+
+//     if (res.length !== 0) {
+//       res.push(arr[j]);
+//       break;
+//     }
+//   }
+
+//   return res;
+// };
+
 const findPairsWithSum = (arr, sum) => {
-  let res = [];
-  const newArr = [...arr];
+  for (let i = 0; i < arr.length; i++) {
+    const numFirst = arr[i];
 
-  for (let j = 0; j < arr.length; j++) {
-    for (let i = 1; i < newArr.length; i++) {
-      if (newArr[i] + arr[j] === sum) {
-        res.push(newArr[i]);
-        break;
-      }
-    }
+    for (let j = i + 1; j < arr.length; j++) {
+      const numSecond = arr[j];
 
-    newArr.shift(newArr[0]);
-
-    if (res.length !== 0) {
-      res.push(arr[j]);
-      break;
+      if (numFirst + numSecond === sum) return [numFirst, numSecond];
     }
   }
 
-  return res;
+  return [];
 };
 
 console.log(findPairsWithSum([3, 5, -4, 8, 11, 1, -1, 6], 10));
